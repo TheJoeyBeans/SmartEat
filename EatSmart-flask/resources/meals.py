5cd9a04a1d3a7b9e5c1b5edc015334f7de44bba0
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-11-19 14:34:44 -0700

    meals GET route is written

---
+++ EatSmart-flask/resources/meals.py
@@ -0,0 +1,14 @@
+import models
+from flask import Blueprint, jsonify, request
+from playhouse.shortcuts import model_to_dict
+
+meal = Blueprint('meals', 'meal')
+
+@meal.route('/', methods=["GET"])
+def get_all_meals():
+	try:
+		meals = [model_to_dict(meal) for meal in models.Meal.select()]
+		print(meals)
+		return josnify(data=meals, status={"code": 200, "message": "Success"})
+	except models.DoesNotExist:
+		return josnify(data={}, status={"code": 401, "message": "Error getting the resources"})
